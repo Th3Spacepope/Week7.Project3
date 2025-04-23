0ed7b7932447ef8b3afeb1b04f78c5829df764a0
Th3Spacepope <tun44465@temple.edu>
2025-04-23 01:20:52 -0400

    Done?

--- script.js
+++ script.js
@@ -8,27 +8,33 @@ const dBtoA = function (linAmp) {
 
 //create oscillator
 let myOsc = audCtx.createOscillator();
-myOsc.type = "sine";
+//myOsc.type = "sine";
 myOsc.frequency.value = 440;
 //myOsc.start();
 
 //create tremolo osc
 let tremOsc = audCtx.createOscillator();
 tremOsc.type = "sine";
-tremOsc.frequency.value = 0;
+tremOsc.frequency.value = 1;
 
 //create gain
 let masterGain = audCtx.createGain();
+masterGain.gain.value = 0.0;
 
 //create tremolo center
 let constSrc = audCtx.createConstantSource();
 constSrc.offset.value = 1;
-
+//create tremolo gain
+let tremGain = audCtx.createGain();
 //create tremolo depth
-let tremDpth = audCtx.createGain;
+let tremDpth = audCtx.createGain();
 
 //connect stuff
-myOsc.connect(masterGain);
+tremOsc.connect(tremGain);
+constSrc.connect(tremGain);
+tremGain.connect(tremDpth.gain);
+myOsc.connect(tremDpth);
+tremDpth.connect(masterGain);
 masterGain.connect(audCtx.destination);
 
 //Functions
@@ -56,12 +62,25 @@ const updateWaveType3 = function () {
   console.log(myOsc.type);
 };
 
+const updateTremDpth = function () {
+  tremGain.gain.exponentialRampToValueAtTime(
+    `${tremDeep.value / 100}`,
+    audCtx.currentTime + 0.01
+  );
+  tremDeepLabel.innerText = `${tremDeep.value}%`;
+};
+const updateTremFreq = function () {
+  tremOsc.frequency.value = `${tremFreq.value}`;
+  tremFreqLabel.innerText = `${tremFreq.value} Hz`;
+};
+
 const start = function () {
   let amp = dBtoA(masterFader.value);
   audCtx.resume();
-  myOsc.start();
-
   masterGain.gain.exponentialRampToValueAtTime(amp, audCtx.currentTime + 0.5);
+  myOsc.start();
+  tremOsc.start();
+  constSrc.start();
 };
 const stop = function () {
   masterGain.gain.exponentialRampToValueAtTime(
@@ -80,6 +99,10 @@ let waveTypeSq = document.getElementById("square");
 let waveTypeTri = document.getElementById("triangle");
 let oscStart = document.getElementById("start");
 let oscStop = document.getElementById("stop");
+let tremDeep = document.getElementById("depth");
+let tremDeepLabel = document.getElementById("depthLabel");
+let tremFreq = document.getElementById("tremFreq");
+let tremFreqLabel = document.getElementById("tremFreqLabel");
 
 //Listeners
 masterFader.addEventListener("input", updateMasterFader);
@@ -89,3 +112,5 @@ waveTypeSq.addEventListener("click", updateWaveType2);
 waveTypeTri.addEventListener("click", updateWaveType3);
 oscStart.addEventListener("click", start);
 oscStop.addEventListener("click", stop);
+tremDeep.addEventListener("input", updateTremDpth);
+tremFreq.addEventListener("input", updateTremFreq);
